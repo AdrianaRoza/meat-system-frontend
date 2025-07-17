90ffc3f4d1c2cbd5d6a1f08ed65752325cf475aa
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-07-17 16:12:53 -0400

    create inputs with value and onChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Layout from "./components/Layout";
-import MeatList from "./components/MeatList";
-
+import Layout from "./components/Layout"
+import MeatList from "./components/MeatList"
+import MeatForm from "./components/MeatForm"
 const App = () => {
   return (
     <Layout>
       <p className="text-center text-gray-700">Em breve a lista de carnes vai aparecer aqui!</p>
-      <MeatList/>
+      <MeatForm/>
+      {/* <MeatList/> */}
     </Layout>
   )
 }
--- src/components/MeatForm.jsx
+++ src/components/MeatForm.jsx
@@ -9,9 +9,80 @@ const MeatForm = () => {
     stock_kg:"",
   })
 
+  const handleChange = (e) => {
+    const { name, value} = e.target
+
+
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }))
+  }
+
   return(
-    <form>
-      {/* Aqui Depois vao os inputs */}
+    <form  
+      className="space-y-4 p-4 bg-white shadow-md rounded max-w-md mx-auto"
+    >
+      <h2 
+        className="text-xl font-bold text-gray-700">
+          Cadastrar Carne
+      </h2>
+
+      <input 
+        type="text"
+        name="name"
+        placeholder="Nome da Carne"
+        value={form.name}
+        onChange={handleChange}
+        className="w-full p-2 border rounded"
+      />
+
+      <input 
+        type="text"
+        name="type"
+        placeholder="Tipo (bovina, suína...)"
+        value={form.type}
+        onChange={handleChange}
+        className="w-full p-2 border rounded"
+      />
+
+      <input 
+        type="number"
+        name="price_per_kg"
+        placeholder="Preço por kg"
+        value={form.price_per_kg}
+        onChange={handleChange}
+        className="w-full p-2 border rounded"
+      />
+
+
+      <input 
+        type="number"
+        name="stock_kg"
+        placeholder="Estoque em kg"
+        value={form.stock_kg}
+        onChange={handleChange}
+        className="w-full p-2 border rounded"
+      />
+      
+      <button
+        type="submit"
+        className="bg-green-600 text-white py-2 px-4 rounded 
+          hover:bg-green-700 transition"
+      >
+        Salvar Carne
+      </button>
+
+        {/* Teste de que esta funcionando */}
+      <div className="mt-4 bg-gray-100 p-4 rounded">
+        <h3 className="font-semibold text-gray-700">Prévia dos dados:</h3>
+        <p><strong>Nome:</strong> {form.name}</p>
+        <p><strong>Tipo:</strong> {form.type}</p>
+        <p><strong>Preço por kg:</strong> {form.price_per_kg}</p>
+        <p><strong>Estoque em kg:</strong> {form.stock_kg}</p>
+      </div>
+      
+
     </form>
   )
 }
