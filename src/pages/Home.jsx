75c4b47e268ed2e08103f122ef374401e7dda738
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-08-12 08:58:16 -0400

    refactor Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,8 @@
-import Button from "../components/Button"
-import Header from "../components/Header"
-
-
- const Home = ({ onNavigate }) => {
-  // Aqui podemos controlar o botão de cadastro flutuante mais pra frente
-
+ const Home = () => {
   return (
-    <div className="min-h-screen bg-orange-50">
-      <Header title="Meat System - Home" />
-      <main className="max-w-3xl mx-auto p-6">
-        <h2 className="text-xl font-semibold mb-6">Choose an option:</h2>
-
-        <div className="flex flex-col gap-4">
-          <Button onClick={() => onNavigate("stock")}>Carnes em Estoque</Button>
-          <Button onClick={() => onNavigate("restock")}>O que repor em estoque</Button>
-          <Button onClick={() => onNavigate("sales")}>Quantidade de carnes vendidas</Button>
-        </div>
-      </main>
+    <div className="text-center mt-10">
+      <h1 className="text-3xl font-bold mb-4">Meat System</h1>
+      <p className="text-lg text-gray-700">Escolha uma opção no menu acima para começar</p>
     </div>
   )
 }
