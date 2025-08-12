2197bdf28dc1bae066ea9e96a3ba84c0e01ed280
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-08-12 10:27:49 -0400

    delete

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
- const Header = ({ title }) => {
-  return (
-    <header className="bg-orange-600 text-white p-4 shadow-md">
-      <h1 className="text-2xl font-bold">{title}</h1>
-    </header>
-  )
-}
-
-export default Header
