1d8ba1535f46a01fe02e6e8a688eeded42eeb72f
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-08-12 10:28:38 -0400

    refactor app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import Meats from './pages/Meats'
-import Sales from './pages/Sales'
-import Restocks from './pages/Restocks'
+
 
 export default function App() {
   return (
     <Router>
-      <nav className="bg-blue-500 p-4 text-white flex gap-4">
+      <nav className="bg-blue-600 p-4 text-white flex gap-4">
         <Link to="/" className="hover:underline">Home</Link>
         <Link to="/meats" className="hover:underline">Carnes</Link>
         <Link to="/sales" className="hover:underline">Vendas</Link>
@@ -17,8 +16,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/meats" element={<Meats />} />
-          <Route path="/sales" element={<Sales />} />
-          <Route path="/restocks" element={<Restocks />} />
+          
         </Routes>
       </main>
     </Router>
